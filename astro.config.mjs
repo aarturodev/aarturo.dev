f109ac3887cfdbc05fd52de366a414e85b4dad11
Anderson Marin <andersonarturomarin@gmail.com>
2024-09-06 02:30:02 -0500

    change to base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://aarturodev.github.io',
-  base: 'aarturo.dev',
+  base: 'aarturodev',
 });
