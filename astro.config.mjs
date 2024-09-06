18fe94c9c3b1f53a7d7c9f4873b4937b28f43355
Anderson Marin <andersonarturomarin@gmail.com>
2024-09-06 02:11:43 -0500

    config file for the gitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://aarturodev.github.io',
+  base: 'aarturo.dev',
 });
