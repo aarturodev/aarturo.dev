494367d0bf69f6c165cf9699ea6c998321c86714
Anderson Marin <andersonarturomarin@gmail.com>
2024-09-06 03:28:45 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aarturodev.github.io',
   integrations: [tailwind()]
   
 });
